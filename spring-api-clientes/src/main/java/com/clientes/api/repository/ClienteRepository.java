3fe76aa95e11972fcc6dc3bb87383b65bc9530d2
Cleiton <cleiton18ads@gmail.com>
2024-01-25 17:52:25 -0300

    Primeiro Commit

---
+++ spring-api-clientes/src/main/java/com/clientes/api/repository/ClienteRepository.java
@@ -0,0 +1,13 @@
+package com.clientes.api.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.clientes.api.model.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Long>{
+
+
+
+}
